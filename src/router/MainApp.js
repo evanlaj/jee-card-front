e3cc0f114c99a5729c18bc85d0ea32f526ee52c7
Evan <evan.lajusticia@gmail.com>
2021-12-24 02:39:09 +0100

    added route to play War minigame

--- src/router/MainApp.js
+++ src/router/MainApp.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import TestApp from '../pages/TestApp'
 import BlackJackApp from '../pages/BlackJackApp'
+import WarApp from '../pages/WarApp'
 
 function MainApp() {
   return (
@@ -9,9 +10,10 @@ function MainApp() {
       {/* ON PEUT METTRE DES CHOSES AVANT */}
       <BrowserRouter>
         <Routes>
-          <Route exact path='/pile' element={<TestApp />}></Route>
-          <Route exact path='/memory' element={<TestApp />}></Route>
-          <Route exact path='/blackjack' element={<BlackJackApp />}></Route>
+          <Route exact path='/pile'      element={<TestApp />}      />
+          <Route exact path='/memory'    element={<TestApp />}      />
+          <Route exact path='/blackjack' element={<BlackJackApp />} />
+          <Route exact path='/war'       element={<WarApp />}       />
         </Routes>
       </BrowserRouter>
       {/* ON PEUT METTRE DES CHOSES APRÈS */}
