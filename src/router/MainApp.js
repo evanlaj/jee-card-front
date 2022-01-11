4dadba3dbeb41f581693c83b694a82dfdc51782d
Evan <evan.lajusticia@gmail.com>
2022-01-11 17:02:51 +0100

    added Header to MainApp

--- src/router/MainApp.js
+++ src/router/MainApp.js
@@ -1,24 +1,44 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import TestApp from '../pages/TestApp'
-import BlackJackApp from '../pages/BlackJackApp'
-import WarApp from '../pages/WarApp'
+import Header from '../components/Header';
+import TestApp from '../pages/TestApp';
+import BlackJackApp from '../pages/BlackJackApp';
+import WarApp from '../pages/WarApp';
 
-function MainApp() {
-  return (
-    <div>
-      {/* ON PEUT METTRE DES CHOSES AVANT */}
-      <BrowserRouter>
-        <Routes>
-          <Route exact path='/pile'      element={<TestApp />}      />
-          <Route exact path='/memory'    element={<TestApp />}      />
-          <Route exact path='/blackjack' element={<BlackJackApp />} />
-          <Route exact path='/war'       element={<WarApp />}       />
-        </Routes>
-      </BrowserRouter>
-      {/* ON PEUT METTRE DES CHOSES APRÈS */}
-    </div>
-  );
+class MainApp extends React.Component {
+  
+  headerOpen = false;
+
+  constructor() {
+    super();
+    this.toggleHeader = this.toggleHeader.bind(this);
+  }
+  
+  toggleHeader() {
+    this.headerOpen = !this.headerOpen;
+    this.forceUpdate();
+    document.activeElement.blur();
+  }
+
+  render () {
+
+    return (
+      <div>
+        {/* ON PEUT METTRE DES CHOSES AVANT */}
+        <BrowserRouter>
+          <Header open={this.headerOpen} buttonAction={this.toggleHeader}/>
+          <Routes>
+            <Route exact path='/pile'      element={<TestApp />}      />
+            <Route exact path='/memory'    element={<TestApp />}      />
+            <Route exact path='/blackjack' element={<BlackJackApp />} />
+            <Route exact path='/war'       element={<WarApp />}       />
+          </Routes>
+        </BrowserRouter>
+        {/* ON PEUT METTRE DES CHOSES APRÈS */}
+      </div>
+    );
+  }
 }
 
 export default MainApp;
