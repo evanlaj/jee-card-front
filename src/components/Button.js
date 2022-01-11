5959ff2f5ba986709037be6160dde140e43bd61d
Evan <evan.lajusticia@gmail.com>
2022-01-11 17:01:58 +0100

    tweaked button behavior

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,14 @@
 import './css/Button.css';
 
 function Card({label = "Label", action = () => null}) {
+
+  function blurAndAction(event) {
+    event.target.blur();
+    action();
+  }
+
   return (
-    <button className="button" onClick={action}>{label}</button>
+    <button className="button" onClick={(e) => blurAndAction(e)}>{label}</button>
   );
 }
 
