edb6d201b2365e4bc2c7f11e293b49102452a4ea
Evan <evan.lajusticia@gmail.com>
2022-01-11 17:02:35 +0100

    finished Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,7 @@
+import { NavLink } from 'react-router-dom';
+
+import Button from './Button';
+
 import './css/Header.css';
 
 function Header({open =  true, buttonAction = () => null}) {
@@ -8,6 +12,13 @@ function Header({open =  true, buttonAction = () => null}) {
             <div className="header-bar bar2"></div>
             <div className="header-bar bar3"></div>
           </button>
+          { open && 
+            <nav>
+              <NavLink to="/war"><Button label="Bataille"/></NavLink>
+              <NavLink to="/blackjack"><Button label="Blackjack"/></NavLink>
+              <NavLink to="/memory"><Button label="Memory"/></NavLink>
+            </nav>
+          }
     </div>
   );
 }
