63990e9750b9997dc47b02f5ff59e2bcfd4fc819
Evan Lajusticia <evan.lajusticia@gmail.com>
2022-01-28 16:41:34 +0100

    changed header to only display available games

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,28 @@
 import { NavLink } from 'react-router-dom';
+import { checkAvailability } from '../_actions/api_actions';
+import React, { useState } from 'react';
 
 import Button from './Button';
 
 import './css/Header.css';
 
 function Header({open =  true, buttonAction = () => null, session = null}) {
+  
+  const [warAvailability, setWarAvailability] = useState(true);
+  const [blackJackAvailability, setBlackJackAvailability] = useState(true);
+  const [memoryAvailability, setMemoryAvailability] = useState(true);
+
+  checkAvailability("War").then((data) => {
+    setWarAvailability(data);
+  });
+
+  checkAvailability("Black Jack").then((data) => {
+    setBlackJackAvailability(data);
+  });
+
+  checkAvailability("Memory").then((data) => {
+    setMemoryAvailability(data);
+  });
   
   let headerButton = 
     <button className="header-button" onClick={buttonAction}>
@@ -24,9 +42,9 @@ function Header({open =  true, buttonAction = () => null, session = null}) {
           {headerButton}
           { open && 
             <nav>
-              <NavLink to="/war"><Button label="Bataille"/></NavLink>
-              <NavLink to="/blackjack"><Button label="Blackjack"/></NavLink>
-              <NavLink to="/memory"><Button label="Memory"/></NavLink>
+              {warAvailability && (<NavLink to="/war"><Button label="Bataille"/></NavLink>)}
+              {blackJackAvailability && (<NavLink to="/blackjack"><Button label="Blackjack"/></NavLink>)}
+              {memoryAvailability && (<NavLink to="/memory"><Button label="Memory"/></NavLink>)}
               {adminButton}
               <NavLink to="/profil"><Button label="Profile"/></NavLink>
             </nav>
